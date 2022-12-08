089d1db5921e0b50408592eadf50139f3f07aee6
datchamcki <vdat1608@gmail.com>
2022-12-08 15:40:50 +0700

    DANH SACH

--- MaTran2.py
+++ MaTran2.py
@@ -9,8 +9,8 @@ tren=0
 duoi=0
 for i in range(0,n):
     for j in range(0,n):
-        if(j<i): duoi+=matrix[i][j]
-        elif(j>i): tren+= matrix[i][j]
+        if(j<n-i-1 ): tren+=matrix[i][j]
+        elif(j>n-i-1): duoi+= matrix[i][j]
 
 if(abs(tren-duoi)<=k):
     print('YES')
