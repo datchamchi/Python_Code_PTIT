2f3247b956e252763c07f55339d7b5dc726133a7
datchamcki <vdat1608@gmail.com>
2022-12-14 23:23:14 +0700

    DANH SACH

--- TimSoLonNhat.py
+++ TimSoLonNhat.py
@@ -1,16 +1,7 @@
+import re
 t= int(input())
 while t>0:
     t-=1
-    x=input()
-    out=[]
-    s=''
-    for i in x:
-        if(i>='0' and i<='9'):
-            s+= i
-        else:
-            if(len(s) >0): out.append(int(s))
-            s=''
-    if(len(s)>0): out.append(int(s))
-    out.sort(reverse=True)
-    print(out[0])
-
+    s= input()
+    ss= re.findall('[0-9]+', s)
+    print(max([int(i) for i in ss]))
